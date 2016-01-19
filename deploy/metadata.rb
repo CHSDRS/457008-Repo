fa582a38d77cb038d3f096112314e6de95e8cda3
CHSDRS <sekhar_au@yahoo.com>
2016-01-19 14:30:03 +0530

    Update metadata.rb

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -19,6 +19,7 @@ depends "mysql"
 depends "opsworks_postgresql"
 depends "opsworks_nodejs"
 depends "opsworks_aws_flow_ruby"
+depends "deploy"
 
 recipe "deploy::aws-flow-ruby", "Deploy an AWS Flow Ruby application"
 recipe "deploy::rails", "Deploy a Rails application"
